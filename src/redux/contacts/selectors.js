34160a25aa997488f86397abce2454226490fc90
Oleksandr Mil <oleksandr.myliutin@gmail.com>
2025-05-12 23:01:02 +0300

    files

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,2 +1,15 @@
-export const selectUser = state => state.auth.user;
-export const selectIsLoggedIn = state => state.auth.isLoggedIn;
+import { createSelector } from "@reduxjs/toolkit";
+
+export const selectContacts = state => state.contacts.items;
+export const selectFilter = state => state.filters.name;
+export const selectError = state => state.contacts.error;
+export const selectLoading = state => state.contacts.isLoading;
+
+export const selectFilteredContacts = state => state.filters.name;
+
+export const selectFilteredContactsByOptionMemo = createSelector(
+    [selectContacts, selectFilteredContacts], 
+    (contacts, filter) => {
+        const normalized = filter.toLowerCase();
+        return contacts.filter(contact => contact.name.toLowerCase().includes(normalized));
+});
