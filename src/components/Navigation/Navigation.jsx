830cf54b8043fd3fb6abef88a115e2473d2699e5
Oleksandr Mil <oleksandr.myliutin@gmail.com>
2025-05-13 22:11:38 +0300

    add navigation refix

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
 import { NavLink } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
 
-const Navigation = () => {
+export default function Navigation() {
+    const isLoggedIn = useSelector(selectIsLoggedIn);
+
     return (
         <>
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/contacts">Contacts</NavLink></li>
-        </>
+        <NavLink to="/" className="btn btn-ghost normal-case text-xl">
+        Home
+        </NavLink>
+
+        {isLoggedIn && (
+        <NavLink to="/contacts" className="btn btn-ghost normal-case text-xl">
+            Contacts
+        </NavLink>
+        )}
+    </>
     );
 }
-
-export default Navigation
